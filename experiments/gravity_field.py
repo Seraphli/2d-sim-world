32ea6124866df685c0797e5c14e3184526a6baaa
SErAphLi <seraphlivery@gmail.com>
2018-08-31 15:14:37 +0800

    use numpy to accelerate

--- experiments/gravity_field.py
+++ experiments/gravity_field.py
@@ -4,52 +4,40 @@ import multiprocessing as mp
 G = 9.8
 
 
-def func(args):
-    _x, y, point_num, mass_point = args
-    z = np.array([0, 0, 1])
-    vals = []
-    for _y in range(y):
-        _val = 0
-        for _i in range(point_num):
-            v = mass_point[_i, :3] - np.array([_x, _y, 0])
-            r = np.linalg.norm(v)
-            if r > 0:
-                v = v / (np.power(r, 3)) * G * mass_point[_i, 3]
-                val = np.dot(v, z)
-                _val += val
-            else:
-                _val += mass_point[_i, 2]
-        vals.append(_val)
-    return vals
-
-
 class GravityField(object):
     def __init__(self, width=1280, height=720):
         self.x = width
         self.y = height
         self.z = 100
         self.m = (5, 50)
-        self.point_num = 30
+        self.point_num = 3
         self.pool = mp.Pool(mp.cpu_count() - 2)
 
     def reset(self):
-        z = np.random.normal(0, self.z / 3, self.point_num)
         m_range = self.m[1] - self.m[0]
         m = np.random.normal(m_range / 2, m_range / 6, self.point_num)
         m = np.abs(m - m_range / 2) + self.m[0]
-        self.mass_point = [
+        mass_point = [
             np.random.randint(0, self.x + 1, self.point_num),
             np.random.randint(0, self.y + 1, self.point_num),
-            z, m]
-        self.mass_point = np.stack(self.mass_point, 1)
-        self.world = np.zeros((self.x, self.y))
-        results = self.pool.map(func, [
-            (i, self.y, self.point_num, self.mass_point)
-            for i in range(self.x)])
-        self.world = np.array(results)
+            np.random.normal(0, self.z / 3, self.point_num)]
+        mass_point = np.stack(mass_point, 1)
+        world = []
+        for _x in range(self.x):
+            for _y in range(self.y):
+                world.append([_x, _y, 0])
+        world = np.reshape(np.array(world), (self.x, self.y, 3))
+        z = np.zeros((self.x, self.y))
+        for n in range(self.point_num):
+            r = np.sqrt(np.sum(np.power(mass_point[n] - world, 2), 2))
+            r_3 = np.repeat(np.reshape(r, (self.x, self.y, 1)), 3, 2)
+            _v = np.divide(np.divide(np.divide(
+                mass_point[n] - world, r_3) * G * m[n], r_3), r_3)
+            z += np.dot(_v, np.array([0, 0, 1]))
+        self.world = z
         self.world = (self.world - np.min(self.world)) / \
                      (np.max(self.world) - np.min(self.world))
-        self.world = (self.world * 0xffffff).astype(np.uint32)
+        self.world = (self.world * 0xff).astype(np.uint32)
 
     def step(self):
         return self.world, {}
